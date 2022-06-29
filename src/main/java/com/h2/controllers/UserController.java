9ee6d633df65b25dd53e55dd7084ce15c5d3d98a
Pratham Meghani <premmeghani16@gmail.com>
2022-06-29 23:34:19 +0530

    AllControllersDone

--- src/main/java/com/h2/controllers/UserController.java
+++ src/main/java/com/h2/controllers/UserController.java
@@ -1,7 +1,13 @@
 package com.h2.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +25,32 @@ public class UserController {
 	public User addUser(@RequestBody User user) {
 	return userService.createUser(user);	
 	}
+	
+	
+	@PostMapping("/addUsers")
+	public List<User> addUsers(@RequestBody List<User> users){
+		return userService.createUsers(users);
+	}
+	
+	
+	@GetMapping("/user/{id}")
+	public User getUserById(@PathVariable int id) {
+	return userService.getUserById(id);	
+	}
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getUsers();	
+	}
+	
+	    @PutMapping("/updateUser")
+		public User updateUser(@RequestBody User user) {
+		return userService.updateUser(user);	
+	}
+	    
+	    @DeleteMapping("/deleteUser/{id}")
+	    public String deleteUser(@PathVariable int id) {
+	    	return userService.deleteUserById(id);
+	    }
+	    
 }
